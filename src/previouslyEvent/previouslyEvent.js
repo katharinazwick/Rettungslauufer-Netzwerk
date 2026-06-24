3a2fbf8ab1e775ee62e353524386d466e7336355
katharinazwick <katharinazwick.71@gmail.com>
2026-06-24 10:10:30 +0200

    include first previouslyEvent

--- src/previouslyEvent/previouslyEvent.js
+++ src/previouslyEvent/previouslyEvent.js
@@ -1,36 +1,10 @@
 export const previouslyEvent =  [
     {
         name: "Erstes Treffen",
-        date: "2026-06-01",
-        place: "...",
-        infos: "Hier ist etwas passiert das toll war",
-        image: "../../css/events/platzhalter.jpg"
-    },
-    {
-        name: "Event 2025",
-        date: "2024-06-01",
-        place: "...",
-        infos: "Hier ist etwas passiert das toll war",
-    },
-    {
-        name: "Platzhalter",
-        date: "2026-05-06",
-        place: "...",
-        infos: "Der stille StellvertreterIch stehe hier, " +
-            "nur kurz – zur Wahl,Bin füllend, doch nicht " +
-            "Inhalt selbst.Ein Zwischenschritt, ein leeres " +
-            "Mal,Das du mit Sinn erst überstellst.Ich halte " +
-            "fest, was kommen soll,Ein Schatten, der den Raum " +
-            "besetzt.Bin selten leer, doch niemals voll,Vom " +
-            "echten Leben stets ersetzt.Kein Name, nur ein Zeichen, " +
-            "lautlos,Ein Warteschleifen-Stellbefehl.Ich lasse los, " +
-            "bin wunschlos, drahtlos,Der leere, stumme, bunte Teil."
-    },
-    {
-        name: "Event 2025",
-        date: "2025-06-01",
-        place: "...",
-        infos: "Hier ist etwas passiert das toll war",
-        image: "../../css/events/patzhalter2.png"
+        date: "2026-06-15",
+        place: "vor dem Blue - das Sportbad, Potsdam",
+        infos: "Bei leicht windigem, aber gutem Wetter haben wir mit einer überschaubaren Gruppe den ersten Lauf gewagt." +
+            " Über 7,07km und 41min ging es einmal um Hermannswerder.",
+        image: "../../css/events/firstVisit.PNG"
     },
 ];

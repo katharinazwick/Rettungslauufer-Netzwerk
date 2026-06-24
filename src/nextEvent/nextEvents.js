31395fb0475cc4b85b7c09945355d6eb254b2f01
katharinazwick <katharinazwick.71@gmail.com>
2026-06-24 11:25:42 +0200

    input

--- src/nextEvent/nextEvents.js
+++ src/nextEvent/nextEvents.js
@@ -1,7 +1,2 @@
 export const nextEvents = [
-    {
-        name: "Erstes Treffen",
-        date: "2026-06-15",
-        place: "vor dem Blu - das Sportbad, Potsdam",
-        infos: "Seid dabei wenn wir gemeinsam eine neue Laufgemeinschaft in Potsdam gründen.\n"},
 ];
